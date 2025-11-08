c6015f18e71f49b8d289e774d944fbecaf5c7e14
Shravan Arunkumar <onetwothreefourfivesixseven73@gmail.com>
2025-11-08 18:09:24 +0000

    Added get_underlying_spot function to utility

--- utility.py
+++ utility.py
@@ -1,1 +1,9 @@
 #test
+import yfinance as yf
+import pandas as pd
+
+def get_underlying_spot(ticker: str) -> float:
+    yf_ticker: yf.Ticker = yf.Ticker(ticker)
+    hist: pd.DataFrame = yf_ticker.history(period="1d")
+    spot: float = hist['Close'].iloc[-1]
+    return spot
